2d2dde0866595ace43fc906dadfa9f370e10f930
Btuturice <btuturice@outlook.com>
2015-10-09 11:01:32 -0400

    h

--- CS_Chapter2/Eggs/Program.cs
+++ CS_Chapter2/Eggs/Program.cs
@@ -9,6 +9,14 @@ namespace Eggs
     {
         static void Main(string[] args)
         {
+            int eggs1 = 20;
+            int eggs2 = 25;
+            int eggs3 = 30;
+            int eggs4 = 35;
+            int total = eggs1 + eggs2 + eggs3 + eggs4;
+            int dozon = total / 12;
+            int eggs = total % 12;
+            Console.WriteLine("dozons: {0} eggs: {1}", dozon, eggs);
         }
     }
 }
